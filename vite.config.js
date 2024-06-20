00eb35c65c809c1612225320b734e3e53f8e64c7
Aziz <abdullah.aziz@dansmultipro.com>
2024-06-20 19:35:09 +0700

    add base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio/",
   resolve: {
     alias: {
       "@components": path.resolve("src", "components"),
