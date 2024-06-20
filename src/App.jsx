c7c2e81143ede8231aec829b21f2c37244c35965
Aziz <abdullah.aziz@dansmultipro.com>
2024-06-20 17:28:19 +0700

    add background animation

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import Summary from "@sections/Summary";
 import styled from "styled-components";
 import GlobalStyle from "./utils/global";
 
+import option from "@utils/particles.json";
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+
 const Container = styled.div`
   position: relative;
 `;
@@ -19,9 +24,27 @@ const MainWrapper = styled.div`
 `;
 
 const App = () => {
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
     <>
       <GlobalStyle />
+      {init && (
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={option}
+        />
+      )}
       <Container>
         <Navigation />
         <MainWrapper>
